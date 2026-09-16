426f6602db6c1a5a62f48d2bf1d406ed063ca8a9
Feliscatus3 <nizarfazari889@gmail.com>
2026-09-16 18:07:43 +0700

    new update firebase api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,30 +164,6 @@ export default async function HomePage({ searchParams }: HomePageProps) {
           title="Jangan Lewatkan Berita Hukum Terkini"
           description="Dapatkan berita hukum, analisis mendalam, dan edukasi hukum langsung di kotak masuk Anda setiap minggu."
         />
-
-        <script type="module">
-          // Import the functions you need from the SDKs you need
-          import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-          import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
-          // TODO: Add SDKs for Firebase products that you want to use
-          // https://firebase.google.com/docs/web/setup#available-libraries
-
-          // Your web app's Firebase configuration
-          // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-          const firebaseConfig = {
-            apiKey: "AIzaSyA4yyjjGzbz5MfI19tsXOYxWn-JPeAawrk",
-            authDomain: "titik-pasal.firebaseapp.com",
-            projectId: "titik-pasal",
-            storageBucket: "titik-pasal.firebasestorage.app",
-            messagingSenderId: "716472953808",
-            appId: "1:716472953808:web:63225f5f72cd54689927d5",
-            measurementId: "G-XSGWSRZCNF"
-          };
-
-          // Initialize Firebase
-          const app = initializeApp(firebaseConfig);
-          const analytics = getAnalytics(app);
-        </script>
       </main>
 
       <Footer />
