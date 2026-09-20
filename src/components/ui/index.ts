6e571a086c0968a15211446926bf49612002d1d8
Feliscatus3 <nizarfazari889@gmail.com>
2026-09-21 02:26:31 +0700

    new update 1

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Input, Textarea, Select } from './Input';
-export { Skeleton, ArticleGridSkeleton, ArticleListSkeleton, HeroSkeleton, CategoryGridSkeleton, LatestNewsSkeleton } from './Skeleton';
-export { EmptyState, EmptySearchState, EmptyArticlesState, EmptyCategoryState, EmptyBookmarksState, EmptyNotificationsState, EmptyAuthorsState } from './EmptyState';
-export { ErrorState, NetworkErrorState, NotFoundState, ForbiddenState, ServerErrorState } from './ErrorState';
--- src/lib/firebase/index.ts
+++
@@ -1,4 +0,0 @@
-export { app, auth, db, storage as firebaseStorage } from './config';
-export * from './auth';
-export * from './firestore';
-export * from './storage';
